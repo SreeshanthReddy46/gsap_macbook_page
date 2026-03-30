5e1b195ba2941f7414eb852a2cf3dfbcef1dbe71
SreeshanthReddy46 <sreeshanthreddy2529@gmail.com>
2026-03-30 23:29:52 +0530

    Implement footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,27 @@
+import {footerLinks} from "../Constants/index.js";
 
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="info">
+                <p>More ways to purchase the better future product to your home......</p>
+                <img src="/logo.svg" alt="logo" />
+            </div>
+
+            <hr />
+            <div className="links">
+                <p>Copy rights from apple.com</p>
+
+                <ul>
+                    {footerLinks.map(({label, link}) =>(
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </footer>
     )
 }
 export default Footer
